076026802e4763018b1358f1088fd901fbafe137
wullemsb <wullemsb@gmail.com>
2017-05-23 22:06:02 +0200

    blog setup

---
+++ README.md
@@ -0,0 +1,88 @@
+# Ordina JWorks Tech Blog
+
+## Running the blog with Docker
+
+    - When running on Windows, make sure your drive is shared with Docker (docker settings)
+    - Execute `docker-compose up -d`
+    - Browse to [http://localhost:4000/](http://localhost:4000/)
+    - After making changes in the blog run, the blog is automatically rebuilt
+
+## Setting up Jekyll on your Mac or Linux
+You need to have at least Ruby 2.4.0 installed, this can be easily accomplished by using [rvm](https://rvm.io).
+
+Execute the following commands:
+
+    $ rvm install 2.4.0
+    $ rvm use 2.4.0
+    $ (sudo) gem install bundler
+    $ bundle install
+
+## Running Jekyll on your Mac or Linux
+- Run `jekyll serve` in the root directory of the tech blog
+- Browse to [http://localhost:4000/](http://localhost:4000/)
+
+## Add yourself as an author
+- Create a **feature branch**, starting from **source**: feature/author-update-john-doe
+- Add yourself to the **_data/authors.yml** file
+- Add a picture of yourself to the **img** folder
+- **Rebase** your changes onto the **source** branch
+- Create a **pull request**, this will be reviewed and merged by one of the competence leaders
+
+## Create a new blog post
+- Create a **feature branch**, starting from **source**: feature/yyyy-mm-dd-john-doe-hello-world
+- Add a blog post image to the **img** directory
+- Use any (online) **Markdown editor** (for example [brackets](http://brackets.io) or [classeur.io](http://classeur.io))
+- Add following [**FrontMatter**](http://jekyllrb.com/docs/frontmatter/) tags on the top of your post (you can also copy-paste this from another post)
+    - layout: post
+    - authors: {author(s)}
+    - title: {title}
+    - image: {image_path}
+    - tags: {tags}
+    - category: {category}
+    - comments: true
+- Write your blog
+- Save your file in the **_post** directory using the following file format: *{year}-{month}-{day}-{title}.md*
+	- **example**: *2015-11-09-Awesome-Blog-Post.md*
+- Be sure to publish as **plain text**! (Jekyll will generate the static HTML for us)
+- Tweak and commit your changes until you feel satisfied with it
+
+## Submit your blog post for publication
+- **Rebase** your changes onto the **source** branch
+- Create a **pull request** and ask people to proof read your new blog post (we don't want to have any spelling mistakes, do we? ;))
+- If changes need to be made, you can just commit and push to the original feature branch
+- When your blog post has been reviewed, one of the competence leaders will approve and merge your pull request.
+- Congratulations! Your blog post is now visible to the whole world! :)
+
+## Style guide
+
+Write a blog post as if you were writing code.
+Adopt the [**one sentence per line** method](https://raw.githubusercontent.com/brandon-rhodes/blog/master/texts/brandon/2012/one-sentence-per-line.rst).
+
+By starting a new line at the end of each sentence,
+and splitting sentences themselves at natural breaks between clauses,
+a text file becomes far easier to edit and version control.
+
+This allows commenting on specific sentences in a pull request.
+If a change is made to a sentence,
+the diff will only show the difference between the old and new sentence,
+and not the complete paragraph.
+
+## SASS usage
+
+When giving layout to components in your blog,
+please take a look in the existing stylesheets if there isn't a class which you can reuse.
+This way we keep our stylesheet files clean.
+
+If you want to add styling,
+please use the SASS files,
+otherwise the SASS compilation process will overwrite your changes.
+
+You can find SASS files in two locations:
+
+- Partial files (beginning with `_`) need to go in the `_sass` directory
+- Main files,
+which need to be picked up by Jekyll and converted to CSS,
+need to go in the `css` directory.
+These files need to begin with two lines of `---` for Jekyll to pick them up for later compilation.
+
+You can read more on Jekyll and SASS integration [here](https://jekyllrb.com/docs/assets/).
---
+++ js/main.js
@@ -0,0 +1,334 @@
+/* ************************ */
+/* Theme name  : Brave      */
+/* Author name : Ashok      */
+/* ************************ */
+
+/* ****************** */
+/* Tooltips & Popover */
+/* ****************** */
+
+$(".b-tooltip").tooltip();
+
+$(".b-popover").popover();
+
+/* ************** */
+/* Magnific Popup */
+/* ************** */
+
+$(document).ready(function() {
+  $('.lightbox').magnificPopup({type:'image'});
+});
+
+/* *************** */
+/* Custom Dropdown */
+/* *************** */
+
+$(document).ready(function(){
+	var hidden = true;
+	$(".b-dropdown").click(function(e){
+		e.preventDefault();
+		if (hidden){
+           $(this).next('.b-dropdown-block').slideToggle(400, function(){hidden = false;});
+      }
+	});
+	$('html').click(function() {
+        if (!hidden) {
+            $('.b-dropdown-block').slideUp();
+            hidden=true;
+        }
+   });
+   $('.b-dropdown-block').click(function(event) {
+        event.stopPropagation();
+   });
+});
+
+/* ************ */
+/* Owl Carousel */
+/* ************ */
+
+$(document).ready(function() {
+	/* Owl carousel */
+	$(".owl-carousel").owlCarousel({
+		slideSpeed : 500,
+		rewindSpeed : 1000,
+		mouseDrag : true,
+		stopOnHover : true
+	});
+	/* Own navigation */
+	$(".owl-nav-prev").click(function(){
+		$(this).parent().next(".owl-carousel").trigger('owl.prev');
+	});
+	$(".owl-nav-next").click(function(){
+		$(this).parent().next(".owl-carousel").trigger('owl.next');
+	});
+});
+
+/* ************* */
+/* Scroll to top */
+/* ************* */
+
+$(document).ready(function() {
+	$(window).scroll(function(){
+		if ($(this).scrollTop() > 200) {
+			$('.totop').fadeIn();
+		} else {
+			$('.totop').fadeOut();
+		}
+	});
+	$(".totop a").click(function(e) {
+		e.preventDefault();
+		$("html, body").animate({ scrollTop: 0 }, "slow");
+		return false;
+	});
+});
+
+/* *************** */
+/* Navigation menu */
+/* *************** */
+
+$(document).ready(function(){
+
+
+	$.fn.menumaker = function(options) {
+
+    var cssmenu = $(this), settings = $.extend({
+        title: "Menu",
+        format: "dropdown",
+        sticky: false
+      }, options);
+
+      return this.each(function() {
+
+		cssmenu.prepend('<div id="menu-button">' + settings.title + '</div>');
+		$(this).find("#menu-button").on('click', function(){
+		  $(this).toggleClass('menu-opened');
+		  var mainmenu = $(this).next('ul');
+		  if (mainmenu.hasClass('open')) {
+			mainmenu.slideUp().removeClass('open');
+		  }
+		  else {
+			mainmenu.slideDown().addClass('open');
+			if (settings.format === "dropdown") {
+			  mainmenu.find('ul').slideDown();
+			}
+		  }
+		});
+
+		cssmenu.find('li ul').parent().addClass('has-sub');
+
+		multiTg = function() {
+		  cssmenu.find(".has-sub").prepend('<span class="submenu-button"></span>');
+		  cssmenu.find('.submenu-button').on('click', function() {
+			$(this).toggleClass('submenu-opened');
+			if ($(this).siblings('ul').hasClass('open')) {
+			  $(this).siblings('ul').removeClass('open').slideUp();
+			}
+			else {
+			  $(this).siblings('ul').addClass('open').slideDown();
+			}
+		  });
+		};
+
+		if (settings.format === 'multitoggle') multiTg();
+		else cssmenu.addClass('dropdown');
+
+
+      });
+	};
+
+	$(".navy").menumaker({
+		title: "Menu",
+		format: "multitoggle"
+	});
+});
+
+/*
+ Solid State by HTML5 UP
+ html5up.net | @ajlkn
+ Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+ */
+
+(function($) {
+
+	"use strict";
+
+	skel.breakpoints({
+		xlarge:	'(max-width: 1680px)',
+		large:	'(max-width: 1280px)',
+		medium:	'(max-width: 980px)',
+		small:	'(max-width: 736px)',
+		xsmall:	'(max-width: 480px)'
+	});
+
+	$(function() {
+
+		var	$window = $(window),
+			$body = $('body'),
+			$header = $('#header'),
+			$banner = $('#banner');
+
+		// Disable animations/transitions until the page has loaded.
+		$body.addClass('is-loading');
+
+		$window.on('load', function() {
+			window.setTimeout(function() {
+				$body.removeClass('is-loading');
+			}, 100);
+		});
+
+		// Fix: Placeholder polyfill.
+		$('form').placeholder();
+
+		// Prioritize "important" elements on medium.
+		skel.on('+medium -medium', function() {
+			$.prioritize(
+				'.important\\28 medium\\29',
+				skel.breakpoint('medium').active
+			);
+		});
+
+		// Header.
+		if (skel.vars.IEVersion < 9)
+			$header.removeClass('alt');
+
+		if ($banner.length > 0
+			&&	$header.hasClass('alt')) {
+
+			$window.on('resize', function() { $window.trigger('scroll'); });
+
+			$banner.scrollex({
+				bottom:		$header.outerHeight(),
+				terminate:	function() { $header.removeClass('alt'); },
+				enter:		function() { $header.addClass('alt'); },
+				leave:		function() { $header.removeClass('alt'); }
+			});
+
+		}
+
+		if ($banner.length > 0 && $body.hasClass('toggle')) {
+			$window.on('resize', function() { $window.trigger('scroll'); });
+
+			$banner.scrollex({
+				bottom: $header.outerHeight() - 100,
+				enter: function() {
+					console.log('enter');
+					// $body.addClass('custom-image');
+					// $body.css("background-image", "url('" + imageHref + "')");
+
+                    $('#header-image').addClass('header-image');
+                    $('#header-image').css("background-image", "url('" + imageHref + "')");
+					$('#over').css("display", "block");
+				},
+				leave: function() {
+					console.log('leave');
+					$body.removeClass('custom-image');
+					$body.css('background-image', '');
+					$('#over').css("display", "none");
+				}
+			});
+
+		}
+
+		// Menu.
+		var $menu = $('#menu');
+
+		$menu._locked = false;
+
+		$menu._lock = function() {
+
+			if ($menu._locked)
+				return false;
+
+			$menu._locked = true;
+
+			window.setTimeout(function() {
+				$menu._locked = false;
+			}, 350);
+
+			return true;
+
+		};
+
+		$menu._show = function() {
+
+			if ($menu._lock())
+				$body.addClass('is-menu-visible');
+
+		};
+
+		$menu._hide = function() {
+
+			if ($menu._lock())
+				$body.removeClass('is-menu-visible');
+
+		};
+
+		$menu._toggle = function() {
+
+			if ($menu._lock())
+				$body.toggleClass('is-menu-visible');
+
+		};
+
+		$menu
+			.appendTo($body)
+			.on('click', function(event) {
+
+				event.stopPropagation();
+
+				// Hide.
+				$menu._hide();
+
+			})
+			.find('.inner')
+			.on('click', '.close', function(event) {
+
+				event.preventDefault();
+				event.stopPropagation();
+				event.stopImmediatePropagation();
+
+				// Hide.
+				$menu._hide();
+
+			})
+			.on('click', function(event) {
+				event.stopPropagation();
+			})
+			.on('click', 'a', function(event) {
+
+				var href = $(this).attr('href');
+
+				event.preventDefault();
+				event.stopPropagation();
+
+				// Hide.
+				$menu._hide();
+
+				// Redirect.
+				window.setTimeout(function() {
+					window.location.href = href;
+				}, 350);
+
+			});
+
+		$body
+			.on('click', 'a[href="#menu"]', function(event) {
+
+				event.stopPropagation();
+				event.preventDefault();
+
+				// Toggle.
+				$menu._toggle();
+
+			})
+			.on('keydown', function(event) {
+
+				// Hide on escape.
+				if (event.keyCode == 27)
+					$menu._hide();
+
+			});
+
+	});
+
+})(jQuery);
