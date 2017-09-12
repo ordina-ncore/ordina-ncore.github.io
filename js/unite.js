d2065b777174488f89b33626768dd08cd42b9a32
Jeroen Rombouts <jeroen.rombouts@ordina.be>
2017-09-12 12:03:23 +0200

    change from hover to click and close

--- js/unite.js
+++ js/unite.js
@@ -2,6 +2,7 @@
     "use strict";
     $(document).ready(function() {
 
+        let currOpenOverlay = undefined;
         const keynote = $('#keynote');
         const serverless = $('#serverless');
         const tbc = $('#TBC');
@@ -10,47 +11,53 @@
         const cqrs = $('#cqrs');
         const reactive = $('#reactive');
 
+        const openOverlay = function(el) {
+            if (currOpenOverlay) {
+                currOpenOverlay.hide();
+            }
+            el.show();
+            currOpenOverlay = el;
+        }
+
+        const closeoverlay = function(el) {
+            el.hide();
+            currOpenOverlay = undefined;
+        }
+
+        const keynoteCloseBtn = $('#keynote .closeoverlay');
+        keynoteCloseBtn.click(() => closeoverlay(keynote));
         const keynoteLink = $('#ln_keynote');
-        keynoteLink.hover(
-            () => keynote.show(),
-            () => keynote.hide()
-        );
+        keynoteLink.click(() => openOverlay(keynote));
 
-
+        const serverlessCloseBtn = $('#serverless .closeoverlay');
+        serverlessCloseBtn.click(() => closeoverlay(serverless));
         const serverlessLink = $('#ln_serverless');
-        serverlessLink.hover(
-            () => serverless.show(),
-            () => serverless.hide()
-        );
+        serverlessLink.click(() => openOverlay(serverless));
 
+        const tbcCloseBtn = $('#TBC .closeoverlay');
+        tbcCloseBtn.click(() => closeoverlay(tbc));
         const tbcLink = $('#ln_tbc');
-        tbcLink.hover(
-            () => tbc.show(),
-            () => tbc.hide()
-        );
+        tbcLink.click(() => openOverlay(tbc));
 
+        const microservicesCloseBtn = $('#microservices .closeoverlay');
+        microservicesCloseBtn.click(() => closeoverlay(microservices));
         const microservLink = $('#ln_microserv');
-        microservLink.hover(
-            () => microservices.show(),
-            () => microservices.hide()
-        );
-        const cqrsLink = $('#ln_CQRS');
-        cqrsLink.hover(
-            () => cqrs.show(),
-            () => cqrs.hide()
-        )
+        microservLink.click(() => openOverlay(microservices));
 
+        const sessie5CloseBtn = $('#sessieTomMichiel .closeoverlay');
+        sessie5CloseBtn.click(() => closeoverlay(sessie5));
         const sessie5Link = $('#ln_sessie5');
-        sessie5Link.hover(
-            () => sessie5.show(),
-            () => sessie5.hide()
-        )
+        sessie5Link.click(() => openOverlay(sessie5));
 
+        const cqrsCloseBtn = $('#cqrs .closeoverlay');
+        cqrsCloseBtn.click(() => closeoverlay(cqrs));
+        const cqrsLink = $('#ln_CQRS');
+        cqrsLink.click(() => openOverlay(cqrs));
+
+        const reactiveCloseBtn = $('#reactive .closeoverlay');
+        reactiveCloseBtn.click(() => closeoverlay(reactive));
         const reactivelink = $('#ln_reactive');
-        reactivelink.hover(
-            () => reactive.show(),
-            () => reactive.hide()
-        );
+        reactivelink.click(() => openOverlay(reactive));
 
     })
 })(jQuery);
