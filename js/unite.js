131fc74a5985cd55b3287daec23f5713346f6dbf
Rombouts <Jeroen.Rombouts@ordina.be>
2017-09-12 10:15:41 +0200

    add overlays w/o pictures

--- js/unite.js
+++ js/unite.js
@@ -3,12 +3,54 @@
     $(document).ready(function() {
 
         const keynote = $('#keynote');
-        keynote.hide();
-        const showFn = () => keynote.show();
-        const hideFn = () => keynote.hide();
+        const serverless = $('#serverless');
+        const tbc = $('#TBC');
+        const microservices = $('#microservices');
+        const sessie5 = $('#sessieTomMichiel')
+        const cqrs = $('#cqrs');
+        const reactive = $('#reactive');
 
         const keynoteLink = $('#ln_keynote');
-        keynoteLink.hover(showFn, hideFn);
+        keynoteLink.hover(
+            () => keynote.show(),
+            () => keynote.hide()
+        );
+
+
+        const serverlessLink = $('#ln_serverless');
+        serverlessLink.hover(
+            () => serverless.show(),
+            () => serverless.hide()
+        );
+
+        const tbcLink = $('#ln_tbc');
+        tbcLink.hover(
+            () => tbc.show(),
+            () => tbc.hide()
+        );
+
+        const microservLink = $('#ln_microserv');
+        microservLink.hover(
+            () => microservices.show(),
+            () => microservices.hide()
+        );
+        const cqrsLink = $('#ln_CQRS');
+        cqrsLink.hover(
+            () => cqrs.show(),
+            () => cqrs.hide()
+        )
+
+        const sessie5Link = $('#ln_sessie5');
+        sessie5Link.hover(
+            () => sessie5.show(),
+            () => sessie5.hide()
+        )
+
+        const reactivelink = $('#ln_reactive');
+        reactivelink.hover(
+            () => reactive.show(),
+            () => reactive.hide()
+        );
 
     })
 })(jQuery);
